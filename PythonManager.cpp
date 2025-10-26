9951bb79516dba0c3584e577927702e4a3f50213
cuter177 <cuterxd177@gmail.com>
2025-10-25 23:48:14 -0600

    Agregar .gitignore adaptado a Riemann 2.0

---
+++ PythonManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Pop90 on 25/10/2025.
+//
+
+#include "PythonManager.h"
---
+++ PythonManager.h
@@ -0,0 +1,16 @@
+//
+// Created by Pop90 on 25/10/2025.
+//
+
+#ifndef PYTHONMANAGER_H
+#define PYTHONMANAGER_H
+
+
+
+class PythonManager {
+
+};
+
+
+
+#endif //PYTHONMANAGER_H
