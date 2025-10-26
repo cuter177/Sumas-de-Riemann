778176f99537497ec10171d3d5e36e4f1204c31e
cuter177 <cuterxd177@gmail.com>
2025-10-26 12:24:32 -0600

    Interfaz"

---
+++ Interfaz/src/main/java/aplication/App.java
@@ -0,0 +1,17 @@
+package aplication;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
